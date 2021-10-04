91ad06bad13f450f08d0c9232fd29f34319e7af4
polina <hello.polina-LA@yandex.ru>
2021-10-04 10:59:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        new MyWindow();
+
+//        String s1 = "";
+//
+//        long startTime = System.nanoTime();
+//        for(int i = 0; i < 300000; i++) {
+//            s1 += "a";
+//        }
+//
+//        long endTime = System.nanoTime();
+//
+//        float result = (endTime - startTime) * 0.000000001f;
+//        System.out.println(result);
+
+//        StringBuilder sb = new StringBuilder();
+//
+//        long startTime = System.nanoTime();
+//        for(int i = 0; i < 300000; i++) {
+//            sb.append("a");
+//        }
+//        long endTime = System.nanoTime();
+//
+//        float result = (endTime - startTime) * 0.000000001f;
+//        System.out.println(result);
+
+
+//        String s1 = "s1";
+//        String s2 = new String("s2");
+//        String s3 = new String(s2);
+//
+//        char[] symbols = { 'A', 'B', 'C', 'D'};
+//        String s4 = new String(symbols);
+//
+//        byte[] bytes = { 65, 66, 67 };
+//        String s5 = new String(bytes);
+//
+//        String s6 = new String(bytes, StandardCharsets.UTF_16);
+//
+//        System.out.println(s1);
+//        System.out.println(s2);
+//        System.out.println(s3);
+//        System.out.println(s4);
+//        System.out.println(s5);
+//        System.out.println(s6);
+//
+//        String s7 = "Десять: " + (5 + 5);
+//        System.out.println(s7);
+//
+//        String s1 = "Hello world";
+//        String s2 = "Hello world";
+//        String s3 = new String(s1);
+//
+//        String s4 = "Hello ";
+//        String s5 = "world";
+//        String s6 = s4 + s5;
+//
+//        Scanner myScanner = new Scanner(System.in);
+//
+//        System.out.println(s1.equals(s2));
+//        System.out.println(s1.equals(s3));
+//        System.out.println(s1.equals(s6));
+//        System.out.println(s1.equals(myScanner.nextLine()));
+    }
+}
